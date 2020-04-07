e64f952bf94a7acb7c91bc9aa64518a71eb5c1c8
Loarlam <messiah02@mail.ru>
2020-04-07 19:16:49 +0300

    Начало заполнения методам AddElements() класса MyDictionary

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -5,14 +5,57 @@
 индексатор для получения значения элемента по указанному индексу и свойство только для чтения для получения общего количества пар элементов.  
 */
 using System;
+using System.ComponentModel;
 
 namespace Task3
 {
-    class Program
+    class MyDictionary<Tkey, TValue>
     {
-        static void Main(string[] args)
+        public void AddElements()
         {
 
+        }
+    }
+
+    class Program
+    {
+        static string twoWordsWithASpacebar;
+        static string[] arrayOfWordsWithoutSpacebar;
+        static MyDictionary<string, string> dictionary;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Пример новых комбинаций для словаря:\napple яблоко orange апельсин");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            twoWordsWithASpacebar = Console.ReadLine();
+            Console.ResetColor();
+
+            if (twoWordsWithASpacebar.Contains(' '))
+            {
+                arrayOfWordsWithoutSpacebar = twoWordsWithASpacebar.Split(' ');
+                dictionary = new MyDictionary<string, string>();
+            }
+
+            else
+                Console.WriteLine("Строка не содержит пробела.");
+
+
+
+            //Console.WriteLine(dictionary["солнце"]);
+            //Console.WriteLine(dictionary["ручка"]);
+            //Console.WriteLine(dictionary["book"]);
+            //Console.WriteLine(dictionary["sun"]);
+            //Console.WriteLine(dictionary["стiл"]);
+            //Console.WriteLine(dictionary["олівець"]);
+            //Console.WriteLine(dictionary["яблуко"]);
+
+            Console.WriteLine("\n" + new string('-', 30) + "\n");
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine(dictionary[i]);
+            //}
         }
     }
 }
